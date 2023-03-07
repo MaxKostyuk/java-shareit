176a158b243fe9dcbc74c9bb83dd4ae3be8b09cd
Max Kostyuk <kostyuk.max@gmail.com>
2023-03-07 23:22:16 +0600

    feat: методы UserController полностью прописаны

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,7 +1,11 @@
 package ru.practicum.shareit.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 /**
@@ -9,25 +13,29 @@ import java.util.List;
  */
 @RestController
 @RequestMapping(path = "/users")
+@RequiredArgsConstructor
+@Validated
 public class UserController {
 
+    private UserService userService;
+
     @PostMapping
-    User create () {
-        return null;
+    public User create (@Valid @RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable @Positive int id) {
+        return userService.getById(id);
     }
 
     @GetMapping
-    User getById() {
-        return null;
+    public List<User> getAll() {
+        return userService.getAll();
     }
 
-    @GetMapping
-    List<User> getAll() {
-        return null;
-    }
-
-    @DeleteMapping
-    void delete() {
-
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable @Positive int id) {
+        userService.delete(id);
     }
 }
