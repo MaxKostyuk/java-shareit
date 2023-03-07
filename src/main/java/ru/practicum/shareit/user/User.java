3c133bda588945d6e30185d8baf8a6c6d77b83ab
Max Kostyuk <kostyuk.max@gmail.com>
2023-03-07 23:13:39 +0600

    feat: добавлены поля для модели User и их ограничения

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,16 @@
 package ru.practicum.shareit.user;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class User {
+
+    private int id;
+    @NotBlank
+    private String name;
+    @Email
+    private String email;
 }
