7de0e61cf7a86d003de1c430816b32b98a4f304c
Max Kostyuk <kostyuk.max@gmail.com>
2023-03-09 00:40:18 +0600

    feat: обновлены классы UserMapper и UserDTO

--- src/main/java/ru/practicum/shareit/user/UserDTO.java
+++ src/main/java/ru/practicum/shareit/user/UserDTO.java
@@ -1,11 +1,13 @@
 package ru.practicum.shareit.user;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
+@Data
 public class UserDTO {
 
     private int id;
--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -2,7 +2,11 @@ package ru.practicum.shareit.user;
 
 public class UserMapper {
 
-    public static UserDTO toItemDTO(User user) {
+    public static UserDTO toUserDTO(User user) {
         return new UserDTO(user.getId(), user.getName(), user.getEmail());
+    }
+
+    public static User toUser(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
