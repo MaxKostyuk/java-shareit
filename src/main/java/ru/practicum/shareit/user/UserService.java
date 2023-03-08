a30bc73339391a1bc0a6b00cbbf3d8b643406161
Max Kostyuk <kostyuk.max@gmail.com>
2023-03-09 00:09:24 +0600

    feat: создан класс UserService

---
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.user;
+
+import java.util.List;
+
+public class UserService {
+    public User create(User user) {
+        return null;
+    }
+
+    public User getById(int id) {
+        return null;
+    }
+
+    public List<User> getAll() {
+        return null;
+    }
+
+    public void delete(int id) {
+
+    }
+}
