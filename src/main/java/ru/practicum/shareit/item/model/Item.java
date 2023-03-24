7df78dd4f00c9cad54ae31a910f152e706d168d5
Max Kostyuk <kostyuk.max@gmail.com>
2023-03-24 18:05:54 +0600

    feat: добавлена поддержка JPA; составлена схема таблицы; размечены как сущности таблицы модели Item и User

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -3,20 +3,32 @@ package ru.practicum.shareit.item.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 /**
  * TODO Sprint add-controllers.
  */
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
+@Entity
+@Table(name = "items", schema = "public")
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "item_name", nullable = false)
     private String name;
+    @Column(nullable = false)
     private String description;
+    @Column(nullable = false)
     private Boolean available;
+    @Column(nullable = false)
     private int ownerId;
 
 }
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -2,7 +2,9 @@ package ru.practicum.shareit.user.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
@@ -11,11 +13,18 @@ import javax.validation.constraints.NotBlank;
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users", schema = "public")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @NotBlank
+    @Column(name = "user_name", nullable = false, unique = true)
     private String name;
     @Email
+    @Column(nullable = false, unique = true)
     private String email;
 }
