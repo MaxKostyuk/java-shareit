b8bffd9500bcb45bc2f5f0cf70cd7653eecd328b
Max Kostyuk <kostyuk.max@gmail.com>
2023-04-24 17:06:14 +0600

    fix: исправлена ошибка в названии класса. Метод getItemRequestById класса ItemRequestRepository теперь работает корректно

--- src/main/java/ru/practicum/shareit/request/storage/ItemRequestRepositoryCustomImpl.java
+++ src/main/java/ru/practicum/shareit/request/storage/ItemRequestRepositoryCustomImpl.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Lazy;
 import ru.practicum.shareit.exception.ElementNotFoundException;
 import ru.practicum.shareit.request.model.ItemRequest;
 
-public class ItemRepositoryCustomImpl implements ItemRequestRepositoryCustom {
+public class ItemRequestRepositoryCustomImpl implements ItemRequestRepositoryCustom {
 
     private final ItemRequestRepository itemRequestRepository;
 
     private static final String ITEM_REQUEST_NOT_FOUND_TEMPLATE = "Item request with id %d not found";
 
-    public ItemRepositoryCustomImpl(@Lazy ItemRequestRepository itemRequestRepository) {
+    public ItemRequestRepositoryCustomImpl(@Lazy ItemRequestRepository itemRequestRepository) {
         this.itemRequestRepository = itemRequestRepository;
     }
 
