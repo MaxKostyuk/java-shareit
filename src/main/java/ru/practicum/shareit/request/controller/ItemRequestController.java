3358fe825c87b2f4248e1af0b55e2c7d6633a918
Max Kostyuk <kostyuk.max@gmail.com>
2023-04-28 12:49:06 +0600

    feat: реализованы тесты для класса ItemRequestController fix: обнаружены ошибки с отсутствием проверок методов create, getRequest, getUserRequest и getOtherRequests класса ItemRequestController

--- src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -24,27 +24,27 @@ public class ItemRequestController {
 
     @PostMapping
     public ItemRequestDTO create(@RequestBody @Valid ItemRequest itemRequest,
-                                 @RequestHeader(name = USER_ID) int userId) {
+                                 @RequestHeader(name = USER_ID) @ Positive int userId) {
         itemRequest.setUserId(userId);
         itemRequest.setCreated(LocalDateTime.now());
         return requestService.create(itemRequest);
     }
 
     @GetMapping
-    public List<ItemRequestDTO> getUserRequests(@RequestHeader(name = USER_ID) int userId) {
+    public List<ItemRequestDTO> getUserRequests(@RequestHeader(name = USER_ID) @Positive int userId) {
         return requestService.getUserRequests(userId);
     }
 
     @GetMapping("/all")
-    public List<ItemRequestDTO> getOtherRequests(@RequestHeader(name = USER_ID) int userId,
+    public List<ItemRequestDTO> getOtherRequests(@RequestHeader(name = USER_ID) @Positive int userId,
                                                  @RequestParam(defaultValue = "0") @PositiveOrZero int from,
                                                  @RequestParam(defaultValue = "10") @Positive int size) {
         return requestService.getOtherRequests(userId, from, size);
     }
 
     @GetMapping("/{id}")
-    public ItemRequestDTO getRequest(@PathVariable int id,
-                                     @RequestHeader(name = USER_ID) int userId) {
+    public ItemRequestDTO getRequest(@PathVariable @Positive int id,
+                                     @RequestHeader(name = USER_ID) @Positive int userId) {
         return requestService.getRequest(id, userId);
     }
 }
