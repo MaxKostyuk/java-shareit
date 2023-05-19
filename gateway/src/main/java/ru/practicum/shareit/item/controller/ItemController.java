29151334fd1db4a05057370db02a41aa6fd76bc2
Max Kostyuk <kostyuk.max@gmail.com>
2023-05-19 15:03:56 +0600

    feat: в ItemController модуля geteway добавлено логгирование

--- gateway/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ import java.util.ArrayList;
 @RequestMapping("/items")
 @RequiredArgsConstructor
 @Validated
+@Slf4j
 public class ItemController {
 
     private static final String USER_ID = "X-Sharer-User-Id";
@@ -26,12 +28,14 @@ public class ItemController {
     @PostMapping
     public ResponseEntity<Object> createItem(@RequestBody @Valid ItemDto itemDto,
                                              @RequestHeader(name = USER_ID) @Positive Long userId) {
+        log.info("Creating item {}, userId={}", itemDto, userId);
         return itemClient.createItem(userId, itemDto);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getItem(@PathVariable @Positive int id,
                                           @RequestHeader(name = USER_ID) @Positive Long userId) {
+        log.info("Getting item with id={}, userId={}", id, userId);
         return itemClient.getItem(userId, id);
     }
 
@@ -39,6 +43,7 @@ public class ItemController {
     public ResponseEntity<Object> getUserItems(@RequestHeader(name = USER_ID) @Positive Long userId,
                                                @RequestParam(defaultValue = "0") @PositiveOrZero int from,
                                                @RequestParam(defaultValue = "10") @Positive int size) {
+        log.info("Getting all items for userId={} with from={}, size={}", userId, from, size);
         return itemClient.getUserItems(userId, from, size);
     }
 
@@ -47,6 +52,7 @@ public class ItemController {
                                               @RequestParam @NotNull String text,
                                               @RequestParam(defaultValue = "0") @PositiveOrZero int from,
                                               @RequestParam(defaultValue = "10") @Positive int size) {
+        log.info("Searching for userId={} items with text={}, from={}, size={}", userId, text, from, size);
         if (text.isBlank())
             return ResponseEntity.ok(new ArrayList<>());
         return itemClient.searchItems(userId, text, from, size);
@@ -56,6 +62,7 @@ public class ItemController {
     public ResponseEntity<Object> updateItem(@RequestBody ItemDto itemDto,
                                              @PathVariable @Positive int id,
                                              @RequestHeader(name = USER_ID) @Positive Long userId) {
+        log.info("Updating itemId={} by userId={} with item params={}", id, userId, itemDto);
         return itemClient.updateItem(itemDto, id, userId);
     }
 
@@ -63,6 +70,7 @@ public class ItemController {
     public ResponseEntity<Object> addComment(@RequestBody @Valid CommentDto commentDto,
                                              @PathVariable @Positive int id,
                                              @RequestHeader(name = USER_ID) @Positive Long userId) {
+        log.info("Creating comment={} for itemId={} by userId={}", commentDto, id, userId);
         return itemClient.addComment(id, userId, commentDto);
     }
 }
